263fe22c25601ed53b7721593e24dfec6ef1b6f3
MyFriendEden <elw.else.elw@gmail.com>
2024-12-30 18:19:45 +0100

    Add project files.

---
+++ Minco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Minco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
